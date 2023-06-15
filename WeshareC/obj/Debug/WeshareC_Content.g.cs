29d39f08b2514a8ec0058d0cc1947a07298762d6
ATec <111857421+DnFFaM@users.noreply.github.com>
2023-06-15 08:33:06 +0200

    Finished till now

--- WeshareC/obj/Debug/WeshareC_Content.g.cs
+++ WeshareC/obj/Debug/WeshareC_Content.g.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("properties/salary.ico")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("appsettings.json")]
 
 
